d6a6b2bda245d36cc4de357312d8466f31bcf047
Jingi Kim <copyx00@gmail.com>
2021-08-09 00:27:15 +0900

    Add my video from camera and mic

--- src/public/js/videoCall.js
+++ src/public/js/videoCall.js
@@ -1,1 +1,72 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraOffBtn = document.getElementById("cameraOff");
+const cameraSelect = document.getElementById("cameras");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((devices) => devices.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      cameraSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function getMedia(deviceId) {
+  const initalConstraints = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initalConstraints
+    );
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+getMedia();
+
+muteBtn.addEventListener("click", () => {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  muteBtn.innerText = muted ? "Mute" : "Unmute";
+  muted = !muted;
+});
+cameraOffBtn.addEventListener("click", () => {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  cameraOffBtn.innerText = cameraOff ? "Turn Camera Off" : "Turn Camera On";
+  cameraOff = !cameraOff;
+});
+
+cameraSelect.addEventListener("input", () => {
+  getMedia(cameraSelect.value);
+});
